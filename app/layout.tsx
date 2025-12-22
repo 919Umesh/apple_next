927e975f579bb85e349802cbf30dbde5938e412a
Umesh <globaltechumesh11@gmail.com>
2025-12-22 17:26:09 +0545

    Restructure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
-import { Sidebar } from "@/components/sidebar/Sidebar";
-import { LayoutWrapper } from "@/components/LayoutWrapper";
+import { Sidebar } from "@/components/layout/sidebar/Sidebar";
+import { LayoutWrapper } from "@/components/layout/LayoutWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
